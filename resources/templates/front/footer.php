b38dc8e5da7561899a606abbe7b3c339741c6b42
Ehab Elshamy <31779219+EhabElshamy@users.noreply.github.com>
2019-05-08 17:10:11 +0200

    Add files via upload

---
+++ resources/templates/front/footer.php
@@ -0,0 +1,26 @@
+
+    <div class="container">
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; <a href="https://www.facebook.com/ehab.elshamy.73">Ehab Elshamy</a> </p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
